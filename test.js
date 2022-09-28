46948ac62785041da70fbae7b93fc949e24dff1c
Luis Fernandes <lfern.mail@gmail.com>
2022-09-28 17:00:19 +0100

    Removed test comment

--- test.js
+++ test.js
@@ -4,7 +4,6 @@ console.log("Hello world!")
 console.log("New code!")
 console.log("More new code!")
 console.log(3+2);
-console.log("Hello world, hello Javascript!")
 let x = 3;
 if (x>2) {
     console.log("Greater than 2! Congratulations!")
